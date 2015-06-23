ceadd8cc6978cca72844f8ac7f3b33879750486c
Matt Campbell <mattica@gmail.com>
2015-06-23 16:20:00 -0700

    yet another attempt to fix cone constructor

--- TessellationAndVoxelizationGeometryLibrary/Primitive Surfaces/Cone.cs
+++ TessellationAndVoxelizationGeometryLibrary/Primitive Surfaces/Cone.cs
@@ -57,59 +57,46 @@ namespace TVGL
             this.Aperture = aperture;
             var faces = ListFunctions.FacesWithDistinctNormals(facesAll);
             var numFaces = faces.Count;
-            var centers = new List<double[]>();
-            double[] center;
+            var axisRefPoints = new List<double[]>();
+            double[] axisRefPoint;
             var n1 = faces[0].Normal.crossProduct(axis);
             var n2 = faces[numFaces - 1].Normal.crossProduct(axis);
             GeometryFunctions.LineIntersectingTwoPlanes(n1, faces[0].Center.dotProduct(n1),
-              n2, faces[numFaces - 1].Center.dotProduct(n2), axis, out center);
-            if (!center.Any(double.IsNaN) && !StarMath.IsNegligible(center))
-                centers.Add(center);
+              n2, faces[numFaces - 1].Center.dotProduct(n2), axis, out axisRefPoint);
+            if (!axisRefPoint.Any(double.IsNaN) && !StarMath.IsNegligible(axisRefPoint))
+                axisRefPoints.Add(axisRefPoint);
             for (int i = 1; i < numFaces; i++)
             {
                 n1 = faces[i].Normal.crossProduct(axis);
                 n2 = faces[i - 1].Normal.crossProduct(axis);
                 GeometryFunctions.LineIntersectingTwoPlanes(n1, faces[i].Center.dotProduct(n1),
-                  n2, faces[i - 1].Center.dotProduct(n2), axis, out center);
-                if (!center.Any(double.IsNaN) && !StarMath.IsNegligible(center))
-                    centers.Add(center);
+                  n2, faces[i - 1].Center.dotProduct(n2), axis, out axisRefPoint);
+                if (!axisRefPoint.Any(double.IsNaN) && !StarMath.IsNegligible(axisRefPoint))
+                    axisRefPoints.Add(axisRefPoint);
             }
-            center = new double[3];
-            center = centers.Aggregate(center, (current, c) => current.add(c));
-            center = center.divide(centers.Count);
+            axisRefPoint = new double[3];
+            axisRefPoint = axisRefPoints.Aggregate(axisRefPoint, (current, c) => current.add(c));
+            axisRefPoint = axisRefPoint.divide(axisRefPoints.Count);
             /*re-attach to plane through origin */
-            var distBackToOrigin = -1 * axis.dotProduct(center);
-            center = center.subtract(axis.multiply(distBackToOrigin));
+            var distBackToOrigin = -1 * axis.dotProduct(axisRefPoint);
+            axisRefPoint = axisRefPoint.subtract(axis.multiply(distBackToOrigin));
             // approach to find  Apex    
             var numApices = 0;
             var apexDistance = 0.0;
             for (int i = 1; i < numFaces; i++)
             {
-                var distToAxis = GeometryFunctions.DistancePointToLine(faces[i].Center, center, axis);
+                var distToAxis = GeometryFunctions.DistancePointToLine(faces[i].Center, axisRefPoint, axis);
                 var distAlongAxis = axis.dotProduct(faces[i].Center);
                 distAlongAxis += distToAxis / Math.Tan(aperture);
-                if (!double.IsNaN(distAlongAxis))
-                {
-                    numApices++;
-                    apexDistance += distAlongAxis;
-                }
+                if (double.IsNaN(distAlongAxis)) continue;
+                numApices++;
+                apexDistance += distAlongAxis;
+            }                               
                 apexDistance /= numApices;
-            }
-            Apex = center.add(axis.multiply(apexDistance));
+            Apex = axisRefPoint.add(axis.multiply(apexDistance));
             /* determine is positive or negative */
             var v2Apex = Apex.subtract(faces[0].Center);
-            if (v2Apex.dotProduct(axis) > 0)
-                Axis = axis.multiply(-1);
-            else Axis = axis;
-            var volume=0.0;
-            foreach (var face in faces)
-            {
-                face.Area = face.Edges[0].Vector.crossProduct(face.Edges[1].Vector).norm2() / 2;
-                volume += face.Area * (face.Normal.dotProduct(face.Vertices[0].Position.subtract(center))) / 3;
-            }
-
-            IsPositive = (volume >= 0.0);
-
+            IsPositive = (v2Apex.dotProduct(axis) > 0);
         }
     }
 }
