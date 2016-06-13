d50fee5a55993f47303f6c13994916f32618a347
Matt Campbell <mattica@gmail.com>
2016-06-13 12:12:00 -0700

    whoops! a few more changes post slice merge

--- TVGLTest/Program.cs
+++ TVGLTest/Program.cs
@@ -78,7 +78,7 @@ namespace TVGL_Test
                 //MiscFunctions.IsSolidBroken(ts[0]);
                 MinimumEnclosure.OrientedBoundingBox(ts[0]);
                 //TestClassification(ts[0]);
-                TVGL_Helix_Presenter.HelixPresenter.Show(ts[0]);
+                TVGL.Presenter.Show(ts[0]);
                 //TestSimplify(ts[0]);
                 //TestSlice(ts[0]);
                 //TestOBB(ts[0], filename);
@@ -122,7 +122,7 @@ namespace TVGL_Test
             Debug.WriteLine("number of vertices = " + ts.NumberOfVertices);
             Debug.WriteLine("number of edges = " + ts.NumberOfEdges);
             Debug.WriteLine("number of faces = " + ts.NumberOfFaces);
-            TVGL_Helix_Presenter.HelixPresenter.Show(ts);
+            TVGL.Presenter.Show(ts);
         }
 
         //private static void TestClassification(TessellatedSolid ts)
