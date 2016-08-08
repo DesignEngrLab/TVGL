f571c90cb97562616d9674b4f41be32b70e3af3d
Matt Campbell <mattica@gmail.com>
2016-08-08 15:11:36 -0700

    needed to add InternalsVisibleTo("TVGLTest") to PresenterDebugger

--- TVGLPresenterDebugger/Properties/AssemblyInfo.cs
+++ TVGLPresenterDebugger/Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Allow tests to access internal classes
+[assembly: InternalsVisibleTo("TVGLTest")]
