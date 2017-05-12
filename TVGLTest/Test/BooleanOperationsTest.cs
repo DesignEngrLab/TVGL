d23c924ca9e4d6764f12ad583037c696a4973f2c
Matt Campbell <mattica@gmail.com>
2017-05-11 17:51:40 -0700

    more fixes to new Clipper addition. I changed the new Clipper enumerators back to the way they were.

--- TVGLTest/Test/BooleanOperationsTest.cs
+++ TVGLTest/Test/BooleanOperationsTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Threading;
+using ClipperLib;
 using StarMathLib;
 using TVGL;
 using TVGL.Clipper;
