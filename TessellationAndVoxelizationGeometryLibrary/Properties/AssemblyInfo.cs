4313f5893ad1fd8d3905dc85a6b0124644283fc2
Matt Campbell <mattica@gmail.com>
2017-07-26 15:16:29 -0700

    just some adjustment to csproj's and file info

--- TessellationAndVoxelizationGeometryLibrary/Properties/AssemblyInfo.cs
+++ TessellationAndVoxelizationGeometryLibrary/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TessellationAndVoxelizationGeometryLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,8 +24,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.17.725")]
+[assembly: AssemblyFileVersion("1.0.17.725")]
 
 // Allow tests to access internal classes
 [assembly: InternalsVisibleTo("TVGLTest")]
