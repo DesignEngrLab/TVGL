70fe30ced35e05fa2c2cdd58d126c1eea5a430d3
micampbell <mattica@gmail.com>
2017-11-23 20:10:23 -0800

    adding a copy of roslyn's hashset with future plans to change it for the purpose of holding 2, 3, 4 level voxels

---
+++ TessellationAndVoxelizationGeometryLibrary/Voxelization/BitHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TVGL.Voxelization
+{
+    internal class BitHelper
+    {
+        private int* bitArrayPtr;
+        private int intArrayLength;
+
+        public BitHelper(int* bitArrayPtr, int intArrayLength)
+        {
+            this.bitArrayPtr = bitArrayPtr;
+            this.intArrayLength = intArrayLength;
+        }
+
+        internal static int ToIntArrayLength(int originalLastIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void MarkBit(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool IsMarked(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TessellationAndVoxelizationGeometryLibrary/Voxelization/HashHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TVGL.Voxelization
+{
+    internal class HashHelpers
+    {
+        internal static int GetMinPrime()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int GetPrime(int m_count)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int ExpandPrime(int m_count)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bool IsPrime(int newSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
