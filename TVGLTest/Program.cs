a814f2666aae912b6650c0fd16f7a4a5098a0dfa
Alan Grier <griera@oregonstate.edu>
2018-12-21 18:42:36 -0800

    Change testing code to include tool depth limit

--- TVGLTest/Program.cs
+++ TVGLTest/Program.cs
@@ -153,7 +153,7 @@ namespace TVGLPresenterDX
             //var dir = new [] { 0.0, 0.4706, -0.8824 }; //Direction of holes in ObliqueHoles
             var neg = vs1.InvertToNewSolid();
             //var erd = neg.ErodeToNewSolid(vs1, dir);
-            var erd = neg.ErodeToNewSolid(vs1, dir, toolDia:20);
+            var erd = neg.ErodeToNewSolid(vs1, dir, toolDia:20, tLimit:60);
             erd.SolidColor = new Color(KnownColors.Magenta);
             Presenter.ShowAndHang(vs1, erd);
             return;
