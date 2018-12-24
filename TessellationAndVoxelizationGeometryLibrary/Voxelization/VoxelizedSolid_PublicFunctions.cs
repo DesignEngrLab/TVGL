d11fe3aedb490765bff3f8974544f57f80577803
Alan Grier <griera@oregonstate.edu>
2018-12-24 15:26:46 -0800

    Implement conical tool end angle optional parameter

--- TessellationAndVoxelizationGeometryLibrary/Voxelization/VoxelizedSolid_PublicFunctions.cs
+++ TessellationAndVoxelizationGeometryLibrary/Voxelization/VoxelizedSolid_PublicFunctions.cs
@@ -1428,6 +1428,10 @@ namespace TVGL.Voxelization
                 case "cone":
                     var angle = toolOptions[1];
                     return GetVoxelsWithinCircle(vox, dir, toolDia / 2);
+                    double angle;
+                    if (toolOptions.Length < 2) angle = 118;
+                    else if (!double.TryParse(toolOptions[1], out angle))
+                        angle = 118;
                 default:
                     return GetVoxelsWithinCircle(vox, dir, toolDia / 2);
             }
