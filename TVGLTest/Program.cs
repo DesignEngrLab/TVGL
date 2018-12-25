92edc4e7f086918fd3627bb06e6c71a2b2d77430
Alan Grier <griera@oregonstate.edu>
2018-12-24 17:50:49 -0800

    Update testing code

--- TVGLTest/Program.cs
+++ TVGLTest/Program.cs
@@ -147,13 +147,13 @@ namespace TVGLPresenterDX
         public static void TestVoxelization(TessellatedSolid ts, string _fileName)
         {
             stopwatch.Start();
-            var vs1 = new VoxelizedSolid(ts,8);
+            var vs1 = new VoxelizedSolid(ts,7);
             //var dir = new [] { 1.0, 1.0, 1.0 };
             var dir = new [] { 1.0, 2.0, 3.0 };
             //var dir = new [] { 0.0, 0.4706, -0.8824 }; //Direction of holes in ObliqueHoles
             var neg = vs1.InvertToNewSolid();
             //var erd = neg.ErodeToNewSolid(vs1, dir);
-            var erd = neg.ErodeToNewSolid(vs1, dir, toolDia:20, tLimit:60);
+            var erd = neg.ErodeToNewSolid(vs1, dir, toolDia:30, toolOptions: new []{"ball", "118"});
             erd.SolidColor = new Color(KnownColors.Magenta);
             Presenter.ShowAndHang(vs1, erd);
             return;
