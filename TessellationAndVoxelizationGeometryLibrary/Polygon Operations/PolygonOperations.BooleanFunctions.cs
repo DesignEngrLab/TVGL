f909e6668eb9d3220e17dfd5ff89ea9f537738ec
Matt Campbell <micampbell@users.noreply.github.com>
2020-06-23 18:18:47 -0700

    fix for getting subtract and xor to work again

--- TessellationAndVoxelizationGeometryLibrary/Polygon Operations/PolygonOperations.BooleanFunctions.cs
+++ TessellationAndVoxelizationGeometryLibrary/Polygon Operations/PolygonOperations.BooleanFunctions.cs
@@ -398,19 +398,13 @@ namespace TVGL.TwoDimensional
             foreach (var intersectionData in intersections)
             {
                 if (intersectionData.Visited || (byte)intersectionData.Relationship < 0b11100) continue;
-                // don't want to start at t intersection that has already been visited or is not an overlap...although I'm not sure the the latter is 
-                // universally true
-                if (switchingOperator)
-                    currentEdge = intersectionData.EdgeA.IndexInList < intersectionData.EdgeB.IndexInList
-                        ? intersectionData.EdgeA
-                        : intersectionData.EdgeB;
-                else
-                {
-                    var cross = intersectionData.EdgeA.Vector.Cross(intersectionData.EdgeB.Vector);
-                    // cross product is from the entering edge to the other. We use the "enteringEdgeA" boolean to flip the sign if we are really entering B
-                    currentEdge = crossProductSign * cross > 0 ? intersectionData.EdgeA : intersectionData.EdgeB;
-                }
-                //cross product does not have expected sign. Instead, the intersection will have  to be entered from the other edge
+
+                var cross = intersectionData.EdgeA.Vector.Cross(intersectionData.EdgeB.Vector);
+                // cross product is from the entering edge to the other. We use the "enteringEdgeA" boolean to flip the sign if we are really entering B
+                currentEdge = crossProductSign * cross > 0 ? intersectionData.EdgeA : intersectionData.EdgeB;
+
+                if (switchingOperator && currentEdge.IndexInList != Math.Max(intersectionData.EdgeA.IndexInList,
+                    intersectionData.EdgeB.IndexInList)) continue;
                 nextStartingIntersection = intersectionData;
                 return true;
             }
