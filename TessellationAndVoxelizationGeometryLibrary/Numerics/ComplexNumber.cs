be42730cb3e9054fa9588924e497208230811e59
Matt Campbell <micampbell@users.noreply.github.com>
2021-06-20 00:12:18 -0700

    okay, I've gone and done it. Added a polynomial solve and a complex number structure

---
+++ TessellationAndVoxelizationGeometryLibrary/Numerics/ComplexNumber.cs
@@ -0,0 +1,318 @@
+using System;
+using System.Globalization;
+
+namespace TVGL.Numerics
+{
+    public readonly struct ComplexNumber
+    {
+        private const double SlerpEpsilon = 1e-6;
+
+        /// <summary>
+        /// Specifies the real-value of the vector component of the ComplexNumber.
+        /// </summary>
+        public readonly double Real;
+        /// <summary>
+        /// Specifies the Imaginary-value of the vector component of the ComplexNumber.
+        /// </summary>
+        public readonly double Imaginary;
+        /// <summary>
+        /// Constructs a ComplexNumber from the given components.
+        /// </summary>
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        /// <summary>
+        /// Calculates the length of the ComplexNumber.
+        /// </summary>
+        /// <returns>The computed length of the ComplexNumber.</returns>
+        public double Length()
+        {
+            return Math.Sqrt(LengthSquared());
+        }
+
+        /// <summary>
+        /// Calculates the length squared of the ComplexNumber. This operation is cheaper than Length().
+        /// </summary>
+        /// <returns>The length squared of the ComplexNumber.</returns>
+        public double LengthSquared()
+        {
+            return Real * Real + Imaginary * Imaginary;
+        }
+
+        /// <summary>
+        /// Divides each component of the ComplexNumber by the length of the ComplexNumber.
+        /// </summary>
+        /// <param name="value">The source ComplexNumber.</param>
+        /// <returns>The normalized ComplexNumber.</returns>
+        public static ComplexNumber Normalize(ComplexNumber value)
+        {
+            ComplexNumber ans;
+            double invNorm = 1.0 / value.Length();
+
+            ans.Real = value.Real * invNorm;
+            ans.Imaginary = value.Imaginary * invNorm;
+
+            return ans;
+        }
+
+        /// <summary>
+        /// Creates the conjugate of a specified ComplexNumber.
+        /// </summary>
+        /// <param name="value">The ComplexNumber of which to return the conjugate.</param>
+        /// <returns>A new ComplexNumber that is the conjugate of the specified one.</returns>
+        public static ComplexNumber Conjugate(ComplexNumber value)
+        {
+            return new ComplexNumber(value.Real, -value.Imaginary);
+        }
+
+
+        /// <summary>
+        /// Flips the sign of each component of the ComplexNumber.
+        /// </summary>
+        /// <param name="value">The source ComplexNumber.</param>
+        /// <returns>The negated ComplexNumber.</returns>
+        public static ComplexNumber Negate(ComplexNumber value)
+        {
+            return new ComplexNumber(-value.Real, -value.Imaginary);
+        }
+
+        /// <summary>
+        /// Flips the sign of each component of the ComplexNumber.
+        /// </summary>
+        /// <param name="value">The source ComplexNumber.</param>
+        /// <returns>The negated ComplexNumber.</returns>
+        public static ComplexNumber operator -(ComplexNumber value) => Negate(value);
+
+
+        /// <summary>
+        /// Adds two ComplexNumbers element-by-element.
+        /// </summary>
+        /// <param name="value1">The first source ComplexNumber.</param>
+        /// <param name="value2">The second source ComplexNumber.</param>
+        /// <returns>The result of adding the ComplexNumbers.</returns>
+        public static ComplexNumber Add(ComplexNumber value1, ComplexNumber value2)
+        {
+            return new ComplexNumber(value1.Real + value2.Real,
+                value1.Imaginary + value2.Imaginary);
+        }
+
+        /// <summary>
+        /// Adds two ComplexNumbers element-by-element.
+        /// </summary>
+        /// <param name="value1">The first source ComplexNumber.</param>
+        /// <param name="value2">The second source ComplexNumber.</param>
+        /// <returns>The result of adding the ComplexNumbers.</returns>
+        public static ComplexNumber operator +(ComplexNumber value1, ComplexNumber value2) => Add(value1, value2);
+
+        /// <summary>
+        /// Subtracts one ComplexNumber from another.
+        /// </summary>
+        /// <param name="value1">The first source ComplexNumber.</param>
+        /// <param name="value2">The second ComplexNumber, to be subtracted from the first.</param>
+        /// <returns>The result of the subtraction.</returns>
+        public static ComplexNumber Subtract(ComplexNumber value1, ComplexNumber value2)
+        {
+            return new ComplexNumber(value1.Real - value2.Real,
+                value1.Imaginary - value2.Imaginary);
+        }
+        /// <summary>
+        /// Subtracts one ComplexNumber from another.
+        /// </summary>
+        /// <param name="value1">The first source ComplexNumber.</param>
+        /// <param name="value2">The second ComplexNumber, to be subtracted from the first.</param>
+        /// <returns>The result of the subtraction.</returns>
+        public static ComplexNumber operator -(ComplexNumber value1, ComplexNumber value2) => Subtract(value1, value2);
+
+
+        /// <summary>
+        /// Multiplies two ComplexNumbers together.
+        /// </summary>
+        /// <param name="value1">The ComplexNumber on the left side of the multiplication.</param>
+        /// <param name="value2">The ComplexNumber on the right side of the multiplication.</param>
+        /// <returns>The result of the multiplication.</returns>
+        public static ComplexNumber Multiply(ComplexNumber value1, ComplexNumber value2)
+        {
+            return new ComplexNumber(value2.Real * value1.Real - value2.Imaginary * value1.Imaginary,
+                value2.Imaginary * value1.Real + value2.Real * value1.Imaginary);
+        }
+
+
+        /// <summary>
+        /// Multiplies two ComplexNumbers together.
+        /// </summary>
+        /// <param name="value1">The ComplexNumber on the left side of the multiplication.</param>
+        /// <param name="value2">The ComplexNumber on the right side of the multiplication.</param>
+        /// <returns>The result of the multiplication.</returns>
+        public static ComplexNumber operator *(ComplexNumber value1, ComplexNumber value2) => Multiply(value1, value2);
+
+        /// <summary>
+        /// Multiplies a ComplexNumber by a scalar value.
+        /// </summary>
+        /// <param name="value1">The source ComplexNumber.</param>
+        /// <param name="value2">The scalar value.</param>
+        /// <returns>The result of the multiplication.</returns>
+        public static ComplexNumber Multiply(ComplexNumber value1, double value2)
+        {
+            return new ComplexNumber(value2 * value1.Real, value2 * value1.Imaginary);
+        }
+
+        /// <summary>
+        /// Multiplies a ComplexNumber by a scalar value.
+        /// </summary>
+        /// <param name="value1">The source ComplexNumber.</param>
+        /// <param name="value2">The scalar value.</param>
+        /// <returns>The result of the multiplication.</returns>
+        public static ComplexNumber operator *(ComplexNumber value1, double value2) => Multiply(value1, value2);
+
+        /// <summary>
+        /// Multiplies a ComplexNumber by a scalar value.
+        /// </summary>
+        /// <param name="value1">The scalar value.</param>
+        /// <param name="value2">The source ComplexNumber.</param>
+        /// <returns>The result of the multiplication.</returns>
+        public static ComplexNumber operator *(double value1, ComplexNumber value2) => Multiply(value2, value1);
+
+        /// <summary>
+        /// Divides a ComplexNumber by another ComplexNumber.
+        /// </summary>
+        /// <param name="value1">The source ComplexNumber.</param>
+        /// <param name="value2">The divisor.</param>
+        /// <returns>The result of the division.</returns>
+        public static ComplexNumber Divide(ComplexNumber value1, double value2)
+        {
+            var oneOverDenom = 1 / value2;
+            return new ComplexNumber(oneOverDenom * value1.Real, oneOverDenom * value1.Imaginary);
+        }
+
+
+        /// <summary>
+        /// Divides a ComplexNumber by another ComplexNumber.
+        /// </summary>
+        /// <param name="value1">The source ComplexNumber.</param>
+        /// <param name="value2">The divisor.</param>
+        /// <returns>The result of the division.</returns>
+        public static ComplexNumber operator /(ComplexNumber value1, double value2) => Divide(value1, value2);
+
+        /// <summary>
+        /// Divides a ComplexNumber by another ComplexNumber.
+        /// </summary>
+        /// <param name="value1">The source ComplexNumber.</param>
+        /// <param name="value2">The divisor.</param>
+        /// <returns>The result of the division.</returns>
+        public static ComplexNumber Divide(ComplexNumber value1, ComplexNumber value2)
+        {
+            var oneOverDenom = 1 / (value2.Real * value2.Real + value2.Imaginary * value2.Imaginary);
+            return new ComplexNumber(oneOverDenom * (value1.Real * value2.Real + value1.Imaginary * value2.Imaginary),
+                oneOverDenom * (value1.Imaginary * value2.Real - value1.Real * value2.Imaginary));
+        }
+
+        /// <summary>
+        /// Divides a ComplexNumber by another ComplexNumber.
+        /// </summary>
+        /// <param name="value1">The source ComplexNumber.</param>
+        /// <param name="value2">The divisor.</param>
+        /// <returns>The result of the division.</returns>
+        public static ComplexNumber operator /(ComplexNumber value1, ComplexNumber value2) => Divide(value1, value2);
+
+
+        /// <summary>
+        /// Returns a boolean indicating whether the two given ComplexNumbers are equal.
+        /// </summary>
+        /// <param name="value1">The first ComplexNumber to compare.</param>
+        /// <param name="value2">The second ComplexNumber to compare.</param>
+        /// <returns>True if the ComplexNumbers are equal; False otherwise.</returns>
+        public static bool operator ==(ComplexNumber value1, ComplexNumber value2)
+        {
+            return value1.Real == value2.Real &&
+                    value1.Imaginary == value2.Imaginary;
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the two given ComplexNumbers are not equal.
+        /// </summary>
+        /// <param name="value1">The first ComplexNumber to compare.</param>
+        /// <param name="value2">The second ComplexNumber to compare.</param>
+        /// <returns>True if the ComplexNumbers are not equal; False if they are equal.</returns>
+        public static bool operator !=(ComplexNumber value1, ComplexNumber value2)
+        {
+            return value1.Real != value2.Real ||
+                    value1.Imaginary != value2.Imaginary;
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the given ComplexNumber is equal to this ComplexNumber instance.
+        /// </summary>
+        /// <param name="other">The ComplexNumber to compare this instance to.</param>
+        /// <returns>True if the other ComplexNumber is equal to this instance; False otherwise.</returns>
+        public bool Equals(ComplexNumber other)
+        {
+            return Real == other.Real &&
+                    Imaginary == other.Imaginary;
+        }
+
+
+        /// <summary>
+        /// Gets a value indicating whether [just a real number] i.e. the imaginary part is zero.
+        /// </summary>
+        /// <value><c>true</c> if [just real]; otherwise, <c>false</c>.</value>
+        public bool JustRealNumber => Imaginary.IsNegligible();
+
+
+        /// <summary>
+        /// Performs an explicit conversion from <see cref="ComplexNumber"/> to <see cref="System.Double"/>.
+        /// </summary>
+        /// <param name="value1">The value1.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static double ToDouble(this ComplexNumber value1)
+        {
+            return value1.Real;
+        }
+
+        /// <summary>
+        /// Performs an explicit conversion from <see cref="ComplexNumber"/> to <see cref="System.Double"/>.
+        /// </summary>
+        /// <param name="value1">The value1.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static explicit operator double(ComplexNumber value1)
+        {
+            return value1.Real;
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the given Object is equal to this ComplexNumber instance.
+        /// </summary>
+        /// <param name="obj">The Object to compare against.</param>
+        /// <returns>True if the Object is equal to this ComplexNumber; False otherwise.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is ComplexNumber ComplexNumber)
+            {
+                return Equals(ComplexNumber);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a String representing this ComplexNumber instance.
+        /// </summary>
+        /// <returns>The string representation.</returns>
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.CurrentCulture, "{0} + {1}i", Real, Imaginary);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return unchecked(Real.GetHashCode() + 1234567890 * Imaginary.GetHashCode());
+        }
+
+    }
+}
---
+++ TessellationAndVoxelizationGeometryLibrary/Numerics/PolynomialSolve.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TVGL.Numerics
+{
+    public static class PolynomialSolve
+    {
+        public static IEnumerable<ComplexNumber> GetRoots(this IEnumerable<double> coefficients)
+        {
+            var coeffList = coefficients as IList<double> ?? coefficients.ToList();
+            if (coeffList.Count > 5) throw new NotImplementedException("These are analytical solve methods which only go" +
+                "up to quartic numbers");
+            if (coeffList.Count == 3) return Quadratic(coeffList);
+            if (coeffList.Count == 4) return Cubic(coeffList);
+            if (coeffList.Count == 5) return Quartic(coeffList);
+        }
+
+        public static IEnumerable<ComplexNumber> Quadratic(this IEnumerable<double> coeffList)
+        {
+            var enumerator = coeffList.GetEnumerator();
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var squaredCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var linearCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var constant = enumerator.Current;
+            return Quadratic(squaredCoeff, linearCoeff, constant);
+        }
+
+        public static IEnumerable<ComplexNumber> Quadratic(double squaredCoeff, double linearCoeff, double constant)
+        {
+            var oneOverDenom = 1 / (2 * squaredCoeff);
+            var radicalTerm = linearCoeff * linearCoeff - 4 * squaredCoeff * constant;  // more commonly known as b^2 - 4ac
+            if (radicalTerm < 0)  // then imaginary roots
+            {
+                radicalTerm = Math.Sqrt(-radicalTerm);
+                radicalTerm *= oneOverDenom;
+                var negBTerm = -oneOverDenom * linearCoeff;
+                yield return new ComplexNumber(negBTerm, -radicalTerm);
+                yield return new ComplexNumber(negBTerm, radicalTerm);
+            }
+            else
+            {
+                radicalTerm = Math.Sqrt(radicalTerm);
+                radicalTerm *= oneOverDenom;
+                var negBTerm = oneOverDenom * linearCoeff;
+                yield return new ComplexNumber(radicalTerm - negBTerm, 0);
+                yield return new ComplexNumber(-radicalTerm - negBTerm, 0);
+            }
+        }
+
+        public static IEnumerable<ComplexNumber> Cubic(this IEnumerable<double> coeffList)
+        {
+            var enumerator = coeffList.GetEnumerator();
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var cubedCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var squaredCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var linearCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var constant = enumerator.Current;
+            return Cubic(cubedCoeff, squaredCoeff, linearCoeff, constant);
+        }
+
+        public static IEnumerable<ComplexNumber> Cubic(double cubedCoeff, double squaredCoeff, double linearCoeff, double offset)
+        {
+            throw new NotImplementedException();
+        }
+        public static IEnumerable<ComplexNumber> Quartic(this IEnumerable<double> coeffList)
+        {
+            var enumerator = coeffList.GetEnumerator();
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var fourthOrderCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var cubedCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var squaredCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var linearCoeff = enumerator.Current;
+            if (enumerator.MoveNext()) throw new ArgumentException("Missing coefficients to solve quadratic.");
+            var constant = enumerator.Current;
+            return Quartic(fourthOrderCoeff, cubedCoeff, squaredCoeff, linearCoeff, constant);
+        }
+        public static IEnumerable<ComplexNumber> Quartic(double fourthOrderCoeff, double cubedCoeff, double squaredCoeff, double linearCoeff, double offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<ComplexNumber> IntersectingConics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
