56ec6ec1a850080ded65f3b03e45a26eb62935b2
micampbell <matt.campbell@oregonstate.edu>
2022-02-17 19:12:04 -0800

    after so much messing with the PriorityQueue, i determined the solution we had was best (BlueRaja's SimplePriorityQueue). The new .NET6 one was good but not as good as BlueRaja's once BlueRaja's was put in .NET6 (as opposed to .Net Framework 4). And the need to edit the queue as we want requires the extra dictionary stuff. So, in order to optimized BlueRaja's, it is brought into here and AgressiveInlining used where appropriate.

---
+++ TessellationAndVoxelizationGeometryLibrary/Miscellaneous Functions/PriorityQueue/GenericPriorityQueueNode.cs
@@ -0,0 +1,40 @@
+﻿
+// The files in this folder are an abbreviated version of BlueRaja's Optimized Priority Queue.
+// https://github.com/BlueRaja/High-Speed-Priority-Queue-for-C-Sharp
+// It was found to not only outperform the PriorityQueue introduced in
+// .NET 6, but it also has capabilities to Update the priority of an 
+// existing state, and Remove a state from the queue.
+// The FastPriorityQueue, the FastPriorityQueueNode, the StablePriorityQueue, the
+// StablePriorityQueueNode have been removed. The Simple one is used only because
+// the reliance on the 'Node classes cannot be produced without having to create
+// a dictionary. This is actually what the SimplePriorityQueue does (although
+// the name implies that it is simpler.
+namespace Priority_Queue
+{
+    public class GenericPriorityQueueNode<TPriority>
+    {
+        /// <summary>
+        /// The Priority to insert this node at.
+        /// Cannot be manually edited - see queue.Enqueue() and queue.UpdatePriority() instead
+        /// </summary>
+        public TPriority Priority { get; protected internal set; }
+
+        /// <summary>
+        /// Represents the current position in the queue
+        /// </summary>
+        public int QueueIndex { get; internal set; }
+
+        /// <summary>
+        /// Represents the order the node was inserted in
+        /// </summary>
+        public long InsertionIndex { get; internal set; }
+
+
+#if DEBUG
+        /// <summary>
+        /// The queue this node is tied to. Used only for debug builds.
+        /// </summary>
+        public object Queue { get; internal set; }
+#endif
+    }
+}
---
+++ TessellationAndVoxelizationGeometryLibrary/Miscellaneous Functions/PriorityQueue/IFixedSizePriorityQueue.cs
@@ -0,0 +1,40 @@
+﻿// The files in this folder are an abbreviated version of BlueRaja's Optimized Priority Queue.
+// https://github.com/BlueRaja/High-Speed-Priority-Queue-for-C-Sharp
+// It was found to not only outperform the PriorityQueue introduced in
+// .NET 6, but it also has capabilities to Update the priority of an 
+// existing state, and Remove a state from the queue.
+// The FastPriorityQueue, the FastPriorityQueueNode, the StablePriorityQueue, the
+// StablePriorityQueueNode have been removed. The Simple one is used only because
+// the reliance on the 'Node classes cannot be produced without having to create
+// a dictionary. This is actually what the SimplePriorityQueue does (although
+// the name implies that it is simpler.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Priority_Queue
+{
+    /// <summary>
+    /// A helper-interface only needed to make writing unit tests a bit easier (hence the 'internal' access modifier)
+    /// </summary>
+    internal interface IFixedSizePriorityQueue<TItem, in TPriority> : IPriorityQueue<TItem, TPriority>
+    {
+        /// <summary>
+        /// Resize the queue so it can accept more nodes.  All currently enqueued nodes are remain.
+        /// Attempting to decrease the queue size to a size too small to hold the existing nodes results in undefined behavior
+        /// </summary>
+        void Resize(int maxNodes);
+
+        /// <summary>
+        /// Returns the maximum number of items that can be enqueued at once in this queue.  Once you hit this number (ie. once Count == MaxSize),
+        /// attempting to enqueue another item will cause undefined behavior.
+        /// </summary>
+        int MaxSize { get; }
+
+        /// <summary>
+        /// By default, nodes that have been previously added to one queue cannot be added to another queue.
+        /// If you need to do this, please call originalQueue.ResetNode(node) before attempting to add it in the new queue
+        /// </summary>
+        void ResetNode(TItem node);
+    }
+}
