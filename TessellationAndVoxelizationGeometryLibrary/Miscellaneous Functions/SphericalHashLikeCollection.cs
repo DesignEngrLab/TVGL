6b3a49db6543d06c2047f16ab12cc99d55b2b75e
mattMedemaLabs <92892320+mattMedemaLabs@users.noreply.github.com>
2023-08-09 14:21:33 -0700

    added to SphericalHashLikeCollection so that radius can also be used

--- TessellationAndVoxelizationGeometryLibrary/Miscellaneous Functions/SphericalHashLikeCollection.cs
+++ TessellationAndVoxelizationGeometryLibrary/Miscellaneous Functions/SphericalHashLikeCollection.cs
@@ -7,10 +7,9 @@ namespace TVGL
 {
     public class SphericalHashLikeCollection<T> : SphericalHashLikeCollection
     {
-
         private readonly List<T> items;
 
-        public SphericalHashLikeCollection(double dotTolerance) : base(dotTolerance)
+        public SphericalHashLikeCollection(bool ignoreRadius, double dotTolerance) : base(ignoreRadius, dotTolerance)
         {
             items = new List<T>();
         }
@@ -27,7 +26,9 @@ namespace TVGL
         }
         private bool AddIfNotPresent(SphericalAnglePair spherical, Vector3 cartesian, T item, out int i)
         {
-            i = BinarySearch(spherical, cartesian, out var matchFound);
+            var radius = ignoreRadius ? 1 : cartesian.Length();
+            i = ignoreRadius ? BinarySearchIgnoreRadius(spherical, cartesian, out var matchFound) :
+                BinarySearchWithRadius(spherical, cartesian, radius, out matchFound);
             if (matchFound)
                 return false;
             if (i == sphericals.Count)
@@ -61,7 +62,9 @@ namespace TVGL
             => TryGet(spherical, spherical.ToVector3(), out item, out matchingAnglePair, out _);
         private bool TryGet(SphericalAnglePair spherical, Vector3 cartesian, out T item, out SphericalAnglePair matchingSpherical, out Vector3 matchingCartesian)
         {
-            var i = BinarySearch(spherical, cartesian, out var matchFound);
+            var radius = ignoreRadius ? 1 : cartesian.Length();
+            var i = ignoreRadius ? BinarySearchIgnoreRadius(spherical, cartesian, out var matchFound) :
+                  BinarySearchWithRadius(spherical, cartesian, radius, out matchFound);
             if (matchFound)
             {
                 item = items[i];
@@ -86,7 +89,9 @@ namespace TVGL
         public new bool Remove(SphericalAnglePair spherical) => Remove(spherical, spherical.ToVector3());
         public new bool Remove(SphericalAnglePair spherical, Vector3 cartesian)
         {
-            var i = BinarySearch(spherical, cartesian, out var matchFound);
+            var radius = ignoreRadius ? 1 : cartesian.Length();
+            var i = ignoreRadius ? BinarySearchIgnoreRadius(spherical, cartesian, out var matchFound) :
+                  BinarySearchWithRadius(spherical, cartesian, radius, out matchFound);
             if (matchFound)
             {
                 sphericals.RemoveAt(i);
@@ -100,17 +105,27 @@ namespace TVGL
 
     public class SphericalHashLikeCollection : ICollection<SphericalAnglePair>
     {
+        private protected readonly bool ignoreRadius;
         private protected readonly double dotTolerance;
         private protected readonly double angleTolerance;
+        private protected readonly double distanceTolerance;
         private protected readonly List<SphericalAnglePair> sphericals;
         private protected readonly List<Vector3> cartesians;
+        private protected readonly List<double> radii;
 
-        public SphericalHashLikeCollection(double dotTolerance)
+        public SphericalHashLikeCollection(bool ignoreRadius, double angleDegreesTolerance)
         {
-            this.dotTolerance = dotTolerance;
-            angleTolerance = Math.Asin(dotTolerance);
+            this.ignoreRadius = ignoreRadius;
+            angleTolerance = Math.PI * angleDegreesTolerance / 180.0;
+            dotTolerance = Math.Cos(angleTolerance);  //cos(angle) should be above this value to be considered a match
+
+            // using the law of cosines the distance tolerance and assuming a unit sphere, the length of the chord
+            // from tolerance, c, would be c^2 = 2(1-cos(angleTolerance)) = 2(1-dotTolerance)
+            distanceTolerance = Math.Sqrt(2 * (1 - dotTolerance));
             sphericals = new List<SphericalAnglePair>();
             cartesians = new List<Vector3>();
+            if (!ignoreRadius)
+                radii = new List<double>();
         }
 
         public int Count => sphericals.Count;
@@ -130,18 +145,24 @@ namespace TVGL
         public bool Add(Vector3 vector) => AddIfNotPresent(new SphericalAnglePair(vector), vector, out _);
         private bool AddIfNotPresent(SphericalAnglePair spherical, Vector3 cartesian, out int i)
         {
-            i = BinarySearch(spherical, cartesian, out var matchFound);
+            var radius = ignoreRadius ? 1 : cartesian.Length();
+            i = ignoreRadius ? BinarySearchIgnoreRadius(spherical, cartesian, out var matchFound) :
+                BinarySearchWithRadius(spherical, cartesian, radius, out matchFound);
             if (matchFound)
                 return false;
             if (i == sphericals.Count)
             {
                 sphericals.Add(spherical);
                 cartesians.Add(cartesian);
+                if (!ignoreRadius)
+                    radii.Add(radius);
             }
             else
             {
                 sphericals.Insert(i, spherical);
                 cartesians.Insert(i, cartesian);
+                if (!ignoreRadius)
+                    radii.Insert(0, radius);
             }
             return true;
         }
@@ -149,7 +170,7 @@ namespace TVGL
         // This binary search is modified/simplified from Array.BinarySearch
         // (https://referencesource.microsoft.com/mscorlib/a.html#b92d187c91d4c9a9)
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private protected int BinarySearch(SphericalAnglePair spherical, Vector3 cartesian, out bool matchFound)
+        private protected int BinarySearchIgnoreRadius(SphericalAnglePair spherical, Vector3 cartesian, out bool matchFound)
         {
             if (sphericals.Count == 0)
             {
@@ -162,7 +183,7 @@ namespace TVGL
             {
                 int i = lo + ((hi - lo) >> 1);
 
-                if (cartesians[i].Dot(cartesian).IsPracticallySame(1, dotTolerance))
+                if (cartesians[i].Dot(cartesian) >= dotTolerance)
                 {
                     matchFound = true;
                     return i;
@@ -173,11 +194,11 @@ namespace TVGL
             }
             int index = lo;
             // the following is a unique feature of this binary search
-            matchFound = ScanHoop(ref index, spherical, cartesian);
+            matchFound = ScanHoopIgnoreRadius(ref index, spherical, cartesian);
             return index;
         }
 
-        private bool ScanHoop(ref int index, SphericalAnglePair spherical, Vector3 cartesian)
+        private bool ScanHoopIgnoreRadius(ref int index, SphericalAnglePair spherical, Vector3 cartesian)
         {
             var i = index;
             // given that the list is sorted in polar angle, we can scan the hoop up to
@@ -185,14 +206,72 @@ namespace TVGL
             // this is particularly important since the azimuth angle has its values near
             // pi and -pi at the same location
             while (++i < sphericals.Count && sphericals[i].PolarAngle - spherical.PolarAngle < angleTolerance)
-                if (cartesians[i].Dot(cartesian).IsPracticallySame(1, dotTolerance))
+            {
+                if (cartesians[i].Dot(cartesian) >= dotTolerance)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+            i = index;
+            while (--i >= 0 && spherical.PolarAngle - sphericals[i].PolarAngle < angleTolerance)
+            {
+                if (cartesians[i].Dot(cartesian) >= dotTolerance)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private protected int BinarySearchWithRadius(SphericalAnglePair spherical, Vector3 cartesian, double radius, out bool matchFound)
+        {
+            if (sphericals.Count == 0)
+            {
+                matchFound = false;
+                return 0;
+            }
+            var lo = 0;
+            var hi = sphericals.Count - 1;
+            while (lo <= hi)
+            {
+                int i = lo + ((hi - lo) >> 1);
+                if (Math.Abs(radius - radii[i]) <= distanceTolerance
+                    && cartesians[i].Dot(cartesian) >= radius * radii[i] * dotTolerance)
+                {
+                    matchFound = true;
+                    return i;
+                }
+                else if (sphericals[i].PolarAngle < spherical.PolarAngle)
+                    lo = i + 1;
+                else hi = i - 1;
+            }
+            int index = lo;
+            // the following is a unique feature of this binary search
+            matchFound = ScanHoopWithRadius(ref index, spherical, cartesian, radius);
+            return index;
+        }
+
+        private bool ScanHoopWithRadius(ref int index, SphericalAnglePair spherical, Vector3 cartesian, double radius)
+        {
+            var i = index;
+            // given that the list is sorted in polar angle, we can scan the hoop up to
+            // the angle tolerance in either direction
+            // this is particularly important since the azimuth angle has its values near
+            // pi and -pi at the same location
+            while (++i < sphericals.Count && sphericals[i].PolarAngle - spherical.PolarAngle < angleTolerance)
+                if (Math.Abs(radius - radii[i]) <= distanceTolerance
+                    && cartesians[i].Dot(cartesian) >= radius * radii[i] * dotTolerance)
                 {
                     index = i;
                     return true;
                 }
             i = index;
             while (--i >= 0 && spherical.PolarAngle - sphericals[i].PolarAngle < angleTolerance)
-                if (cartesians[i].Dot(cartesian).IsPracticallySame(1, dotTolerance))
+                if (Math.Abs(radius - radii[i]) <= distanceTolerance
+                    && cartesians[i].Dot(cartesian) >= radius * radii[i] * dotTolerance)
                 {
                     index = i;
                     return true;
@@ -210,7 +289,15 @@ namespace TVGL
         public bool Contains(SphericalAnglePair spherical) => Contains(spherical, spherical.ToVector3());
         public bool Contains(SphericalAnglePair spherical, Vector3 cartesian)
         {
-            BinarySearch(spherical, cartesian, out var matchFound);
+            bool matchFound;
+            if (ignoreRadius)
+                BinarySearchIgnoreRadius(spherical, cartesian, out matchFound);
+            else
+            {
+                var radius = cartesian.Length();
+                BinarySearchWithRadius(spherical, cartesian, radius, out matchFound);
+
+            }
             return matchFound;
         }
 
@@ -233,7 +320,9 @@ namespace TVGL
         public bool Remove(SphericalAnglePair spherical) => Remove(spherical, spherical.ToVector3());
         public bool Remove(SphericalAnglePair spherical, Vector3 cartesian)
         {
-            var i = BinarySearch(spherical, cartesian, out var matchFound);
+            var radius = ignoreRadius ? 1 : cartesian.Length();
+            var i = ignoreRadius ? BinarySearchIgnoreRadius(spherical, cartesian, out var matchFound) :
+                    BinarySearchWithRadius(spherical, cartesian, radius, out matchFound);
             if (matchFound)
             {
                 sphericals.RemoveAt(i);
