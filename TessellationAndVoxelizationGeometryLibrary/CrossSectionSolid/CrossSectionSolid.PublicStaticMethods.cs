86b7d82492a13578e4bd0732a9177921bcb881b1
mattMedemaLabs <92892320+mattMedemaLabs@users.noreply.github.com>
2024-04-17 10:58:00 -0700

    small fix that didn't show up during last change

--- TessellationAndVoxelizationGeometryLibrary/CrossSectionSolid/CrossSectionSolid.PublicStaticMethods.cs
+++ TessellationAndVoxelizationGeometryLibrary/CrossSectionSolid/CrossSectionSolid.PublicStaticMethods.cs
@@ -76,7 +76,7 @@ namespace TVGL
             //stepDistances[i] = stepDistances[i - 1] + stepSize;
             var bounds = new[] { ts.Bounds[0].Copy(), ts.Bounds[1].Copy() };
 
-            var layers = ts.GetUniformlySpacedCrossSections(direction, out _, stepDistances[0], numberOfLayers, stepSize);
+            var layers = ts.GetUniformlySpacedCrossSections(direction, out _, out _, stepDistances[0], numberOfLayers, stepSize);
             var layerDict = new Dictionary<int, IList<Polygon>>();
             for (int i = 0; i < layers.Length; i++)
                 layerDict.Add(i, layers[i]);
